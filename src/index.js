54c9bb8fec6ca1e7e0ed9628170d42a0a409ceb5
Noah Connors <noahconnors@mnmsa.org>
2018-07-11 22:08:09 -0500

    Add basic parameter-based pages

--- src/index.js
+++ src/index.js
@@ -5,5 +5,9 @@ app.get('/', (req, res) => {
   res.send('Fighting Calculators');
 });
 
+app.get('/user/:name/', (req, res) => {
+  res.send("Hello there, " + req.params.name);
+});
+
 app.listen(8000, () => console.log('App running on port 8000'));
 
