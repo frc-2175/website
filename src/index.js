68a510868d260ec848912348f835fd58a0a2e263
Noah Connors <noahconnors@mnmsa.org>
2018-07-11 21:55:14 -0500

    Change the words to something a little more reasonable

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const Express = require('express');
 const App = Express();
 
 App.get('/', (req, res) => {
-  res.send('<html><head><title>LOL A WEBSITE</title></head><body><h1>LOL</h1></body></html>');
+  res.send('Fighting Calculators');
 });
 
 App.listen(8000, () => console.log('App running on port 8000'));
