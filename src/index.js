012eef78530a57f33d2b4de6afa8f786ea577b27
Noah Connors <noahconnors@mnmsa.org>
2018-07-12 11:46:28 -0500

    Changed template directory to path from repo root

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const nunjucks = require('nunjucks');
 const app = express();
 
-nunjucks.configure('templates', {
+nunjucks.configure('src/templates', {
   autoescape: true,
   express: app
 });
