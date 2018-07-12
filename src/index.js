e693510649dab1a2f54720bfafb8b95b5073a8ca
Noah Connors <noahconnors@mnmsa.org>
2018-07-11 22:01:12 -0500

    Change capitalization to be cooler

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-const Express = require('express');
-const App = Express();
+const express = require('express');
+const app = express();
 
-App.get('/', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Fighting Calculators');
 });
 
-App.listen(8000, () => console.log('App running on port 8000'));
+
+
+app.listen(8000, () => console.log('App running on port 8000'));
 
