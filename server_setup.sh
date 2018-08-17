577d5c6c35a5fac46de869ceac3813192323e75b
Noah Connors <noahconnors@mnmsa.org>
2018-08-16 19:29:21 -0500

    Add server configuration stuff

---
+++ server_setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Install node
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+# Install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install -y yarn
+# Install caddy
+curl https://getcaddy.com | bash -s personal
+
+# Clone website
+cd /home/ubuntu
+git clone https://github.com/frc-2175/website
+cd website
+yarn install
