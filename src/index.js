5c461e1afe113eeb5448bde14e5f79e02b52a4b7
Noah Connors <noahconnors@mnmsa.org>
2018-10-12 19:53:48 -0500

    Add the team organization page

--- src/index.js
+++ src/index.js
@@ -27,22 +27,26 @@ app.get('/team-organization', (req, res) => {
 
 app.get('/team-organization/awards', (req, res) => {
   res.render('subteams/awards.html');
-})
+});
 
 app.get('/team-organization/build', (req, res) => {
   res.render('subteams/build.html');
-})
+});
 
 app.get('/team-organization/CAD', (req, res) => {
   res.render('subteams/CAD.html');
-})
+});
 
 app.get('/team-organization/electronics', (req, res) => {
   res.render('subteams/electronics.html');
-})
+});
 
 app.get('/team-organization/scouting-and-strategy', (req, res) => {
   res.render('subteams/scouting-and-strategy.html');
-})
+});
+
+app.get('/team-history', (req, res) => {
+  res.render('team-history.html');
+});
 
 app.listen(8000, () => console.log(`App running on port 8000 in ${app.get('env')} mode`));
