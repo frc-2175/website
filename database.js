92076c5cd70be0d0159057e177f28f5014f6b18b
Noah Connors <noahconnors@mnmsa.org>
2018-12-01 15:35:52 -0600

    Add a database creation script

---
+++ database.js
@@ -0,0 +1,65 @@
+const sqlite = require('sqlite3');
+const db = new sqlite.Database('database');
+
+const commands = [
+  `CREATE TABLE users(
+    username TEXT,
+    hash TEXT,
+    PRIMARY KEY(username)
+  );`,
+  'ALTER TABLE users ADD COLUMN food TEXT'  
+];
+
+const callbacks = [
+  function(err) {
+    if(err !== undefined) {
+      console.log(err.message);
+    }
+    console.log('Created users table');
+  },
+  function(err) {
+    if(err !== undefined) {
+      console.log(err.message);
+    }
+    console.log('Created food column');
+  }
+]
+
+db.serialize(function() {
+  db.run(`CREATE TABLE metadata(
+      version INTEGER
+    );`, function(error) {
+      if(error !== null) {
+        console.log('Table metadata already exists');
+      }
+    }
+  );
+  db.get(`SELECT version FROM metadata`, function(error, row) {
+    let start;
+    if(row === undefined) {
+      db.run(`INSERT INTO metadata VALUES (0)`, function(error) {
+        if(error !== null) {
+          console.log('Inserting version number failed');
+          process.exit(1);
+        }
+      });
+      start = 0;
+    } else {
+      start = row.version;
+    }
+    console.log(start);
+    doCommandsFrom(start);
+    db.run(`UPDATE metadata SET version = ${commands.length}`);
+  });
+});
+
+function doCommandsFrom(index) {
+  if(index < commands.length) {
+    db.run(commands[index], function() {
+      callbacks[index]();
+      if(index < commands.length - 1) {
+        doCommandsFrom(index + 1);
+      }
+    });
+  }
+}
