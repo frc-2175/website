2235f9a4c07e58c4de94395df68ee86c042e18a8
Noah Connors <noahconnors@mnmsa.org>
2018-12-29 13:01:31 -0600

    Add documentation to the database.js file

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const sqlite = require('sqlite3');
 const db = new sqlite.Database('database');
 
+// This is a list of commands to be run to create the database schema
 const commands = [
   `CREATE TABLE users(
     username TEXT,
@@ -9,6 +10,10 @@ const commands = [
   );`,
 ];
 
+/* This is a list of callback functions to handle errors that might come up
+   with the commands being run. Callback functions correspond to the command
+   at the same index in the commands[] array.
+   */
 const callbacks = [
   function(err) {
     if(err) {
@@ -18,7 +23,9 @@ const callbacks = [
   }
 ]
 
+// Serializes the following database commands
 db.serialize(function() {
+  // Tries to create the metadata table for holding the version number
   db.run(`CREATE TABLE metadata(
       version INTEGER
     );`, function(error) {
@@ -27,6 +34,14 @@ db.serialize(function() {
       }
     }
   );
+
+  /* Once the metadata table confidently exists, we try to get the version 
+     number from the table. If that fails, we assign 0 as the version since
+     failure means that there was no version number. If that was successful,
+     we store the version number and we execute all of the commands and 
+     callbacks in their respective arrays from that version number as the 
+     starting index.
+     */
   db.get(`SELECT version FROM metadata`, function(error, row) {
     let start;
     if(row === undefined) {
@@ -45,6 +60,10 @@ db.serialize(function() {
   });
 });
 
+/**
+ * Runs commands from the commands array with their respective callbacks
+ * @param {number} index the index at which to start the commands
+ */
 function doCommandsFrom(index) {
   if(index < commands.length) {
     db.run(commands[index], function(err) {
