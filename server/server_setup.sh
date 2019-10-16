4ac80b44c3f3d039d303c51dfd4ab4a6cac203b3
nupurkumark <kumarnupurk@gmail.com>
2019-10-15 19:53:16 -0500

    Update to node 12

--- server/server_setup.sh
+++ server/server_setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Install node
-curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
 # Install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
